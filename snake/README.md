6f8738694ba275a53ed371feae73ef23a68f2cde
diegorezm <dmourarezende@gmail.com>
2024-12-15 15:10:09 -0300

    snake game

---
+++ snake/README.md
@@ -0,0 +1,3 @@
+# Snake game v1
+This is a very simple snake game made with raylib. As you can see its only one .c file, 
+i plan on making a more complex one in the future tho.
--- snake/main.c
+++ snake/main.c
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,10 +12,9 @@ struct snake {
 
 struct fruit {
   Vector2 pos;
-  int32_t size;
-};
+} fruit;
 
-Vector2 generate_random_pos(int boundaries_x, int boundaries_y) {
+Vector2 generateRandomPos(int boundaries_x, int boundaries_y) {
   Vector2 response;
 
   srand(time(NULL));
@@ -24,14 +24,28 @@ Vector2 generate_random_pos(int boundaries_x, int boundaries_y) {
   return response;
 }
 
+bool checkCollision(Vector2 p1, Vector2 p2) {
+  float x = fabs(p1.x - p2.x);
+  float y = fabs(p1.y - p2.y);
+
+  if (x <= 8 && y <= 15)
+    return true;
+  return false;
+}
+
 int main() {
   const int screenWidth = 800;
   const int screenHeight = 450;
-  const int walkSpeed = 10;
+  const int walkSpeed = 20;
 
   snake.pos.x = 0;
   snake.pos.y = 50;
   snake.size = 1;
+
+  Vector2 randomPos = generateRandomPos(screenHeight, screenHeight);
+
+  fruit.pos.x = randomPos.x;
+  fruit.pos.y = randomPos.y;
 
   int score = 0;
 
@@ -61,16 +75,26 @@ int main() {
       snakeDots[i] = '.';
     }
 
-    Vector2 randomPos = generate_random_pos(screenWidth, screenHeight - 5);
     snakeDots[snake.size] = '\0';
     char scoreText[50];
     snprintf(scoreText, sizeof(scoreText), "Score: %d", score);
 
+    if (checkCollision(snake.pos, fruit.pos)) {
+      score++;
+      snake.size++;
+      Vector2 randomPos = generateRandomPos(screenHeight, screenHeight);
+
+      fruit.pos.x = randomPos.x;
+      fruit.pos.y = randomPos.y;
+    }
+
     BeginDrawing();
     ClearBackground(RAYWHITE);
+
     DrawText(scoreText, 5, 5, 30, BLACK);
     DrawText(snakeDots, snake.pos.x, snake.pos.y, 40, BLACK);
-    DrawText("+", randomPos.x, randomPos.y, 40, BLACK);
+    DrawText("+", fruit.pos.x, fruit.pos.y, 30, BLACK);
+
     EndDrawing();
   }
   CloseWindow();
